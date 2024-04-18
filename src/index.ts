438af8c87e3166a4a5e0617706bbc2e5b5806b29
ysdragon <youssefelkholey@gmail.com>
2024-04-18 20:30:19 +0200

    Used `@distube/ytdl-core` instead of broken `ytdl-core`

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { command, streamLivestreamVideo, MediaUdp, setStreamOpts, streamOpts, St
 import config from "./config.json";
 import fs from 'fs';
 import path from 'path';
-import ytdl from 'ytdl-core';
+import ytdl from '@distube/ytdl-core';
 import yts from 'play-dl';
 import { TiktokVideo, TiktokLive } from "./util/Tiktok";
 
