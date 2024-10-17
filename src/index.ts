86cc2b9d316d313e37d818aa48a31e807c3f7f33
ysdragon <youssefelkholey@gmail.com>
2024-10-18 02:07:47 +0300

    Add advanced options

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,26 @@ const streamOpts: StreamOptions = {
     bitrateKbps: config.bitrateKbps,
     maxBitrateKbps: config.maxBitrateKbps,
     hardwareAcceleratedDecoding: config.hardwareAcceleratedDecoding,
-    videoCodec: config.videoCodec === 'VP8' ? 'VP8' : 'H264'
+    videoCodec: config.videoCodec === 'VP8' ? 'VP8' : 'H264',
+    
+    /**
+     * Advanced options
+     * 
+     * Ffmpeg will read frames at native framerate. Disabling this make ffmpeg read frames as
+     * fast as possible and `setTimeout` will be used to control output fps instead. Enabling this
+     * can result in certain streams having video/audio out of sync
+     */
+    readAtNativeFps: false,
+    /**
+     * Enables sending RTCP sender reports. Helps the receiver synchronize the audio/video frames, except in some weird
+     * cases which is why you can disable it
+     */
+    rtcpSenderReportEnabled: false,
+    /**
+     * Encoding preset for H264 or H265. The faster it is, the lower the quality
+     * Available presets: ultrafast, superfast, veryfast, faster, fast, medium, slow, slower, veryslow
+     */
+    h26xPreset: 'ultrafast'    
 };
 
 // Create the videosFolder dir if it doesn't exist
